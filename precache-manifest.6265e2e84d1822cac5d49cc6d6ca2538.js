0a392c05cb761e1e0d00189d4dec5f3a729123e2
The_Silent <thesilent990@gmail.com>
2019-03-16 20:04:54 +0200

    adding react files

---
+++ precache-manifest.6265e2e84d1822cac5d49cc6d6ca2538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "180de0abca294f63e1f8",
+    "url": "/static/css/main.bb81b31d.chunk.css"
+  },
+  {
+    "revision": "180de0abca294f63e1f8",
+    "url": "/static/js/main.1193c345.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fc0a232c0b2a092f5f544fec09a528e2",
+    "url": "/index.html"
+  }
+];
